118e28133fe2eab541235e462905550c6479e1df
Kristofer Lindberg <k.lindberg@gmail.com>
2015-05-15 19:44:58 +0200

    crude but works somewhat

--- call_me_al/gps.py
+++ call_me_al/gps.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
+
 state = ['son-at-home', 'car-needs-battery', 'have-money', 'have-a-phone-book']
+#state = ['son-at-home', 'car-needs-battery', 'have-money']
 
 ops = [{'action': 'drive-son-to-school',
         'preconds': ['son-at-home', 'car-works'],
@@ -17,20 +20,13 @@ ops = [{'action': 'drive-son-to-school',
         'add-list': ['in-communication-with-shop'],
         'del-list': []},
     {'action': 'look-up-number',
-        'preconds': ['have-phone-book'],
+        'preconds': ['have-a-phone-book'],
         'add-list': ['know-phone-number'],
         'del-list': []},
     {'action': 'give-shop-money',
         'preconds': ['have-money'],
         'add-list': ['shop-has-money'],
         'del-list': ['have-money']}]
-
-def GPS(goals):
-    for goal in goals:
-        achieve(goal)
-
-def achieve(goal):
-    return (goal in state) or apply_op(find_all_ops(goal))
 
 def find_all_ops(goal):
     result = []
@@ -39,19 +35,40 @@ def find_all_ops(goal):
             result.append(op)
     return result
 
-def apply_op(ops):
+def GPS(goals):
+    print("we are trying to solve", goals)
+    counter = 1
+    for goal in goals:
+        while not achieve(goal):
+            if counter > 20:
+                print("we cannot solve this within 20 steps")
+                return
+            print("trying again")
+            counter += 1
+
+def achieve(goal):
+    # I'll achieve a goal by meeting all the preconditions
+    print("Achieving", goal)
+    if goal in state:
+        print("We have achieved", goal)
+        return True
+    else:
+        do_these = find_all_ops(goal)
+        for op in do_these:
+            do(op)
+
+def do(op):
     global state
-    for op in ops:
-        for goal in op['preconds']:
-            if not achieve(goal):
-                for d in op['del-list']:
-                    try:
-                        state.remove(d)
-                    except ValueError:
-                        pass
-                for a in op['add-list']:
-                    if a not in state:
-                        state.append(a)
+    if all(achieve(goal) == True for goal in op['preconds']):
+        print("Executing", op['action'])
+        for a in op['add-list']:
+            if a not in state:
+                state.append(a)
+        for d in op['del-list']:
+            try:
+                state.remove(d)
+            except ValueError:
+                pass
 
 GPS(['son-at-school'])
 print(state)
